654d88f77c4dfca79d115d821f842ea106298c6b
sergkit <kitserg68@gmail.com>
2016-01-06 00:49:27 +0300

    day_commit

---
+++ src/AppBundle/Form/setForm.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * Сервер домашней автоматизации.
+ * author kitserg68@gmail.com
+ *
+ */
+
+namespace AppBundle\Form;
+
+/**
+ * Description of AddData
+ *
+ * @author benjuchis
+ */
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AddData extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('room', 'number');
+        $builder->add('co2', 'number');
+        $builder->add('t', 'number');
+        $builder->add('h', 'number');
+        $builder->add('voc', 'number');
+        $builder->add('vocr', 'number');
+        $builder->add('vocold', 'number');
+        $builder->add('CheckFields', 'number');
+        $builder->add('save', 'submit', array('label' => 'Добавить'));
+    }
+
+    public function getName() {
+        return 'add_data';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'csrf_protection' => false,
+        ));
+    }
+
+}
